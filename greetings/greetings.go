a1dc478488f474caa0b5cdbe7a3db77b7496d4e3
Vincent Finley <vwfinley@comcast.net>
2024-11-15 16:06:56 +0000

    module tutorial: error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,10 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	//Return a greeting that embeds a name in a message.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a values that embeds the name.
+	// in a message greeting.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
